997141bec0326bd7ac89dc42dd288f4f28df9ea3
Piyush Yadav <piyushyadav20045@gmail.com>
2026-01-30 22:12:44 +0530

    ui: create hero section and add font variables in globals.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Instrument_Serif } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+});
+
+const instrumentSerif = Instrument_Serif({
+  variable: "--font-instrument-serif",
+  subsets: ["latin"],
+  weight: ["400"],
+  display: "swap",
 });
 
 const geistMono = Geist_Mono({
@@ -25,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${instrumentSerif.variable}  antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import Hero from "@/components/layout/hero";
 
 export default function Home() {
   return (
     <div>
-      next app
+      <Hero />
     </div>
   );
 }
